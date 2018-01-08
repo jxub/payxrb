98fa85eb5be54189609941a38e879936fc05a86d
Jakub Janarek <jjanarek@gmail.com>
2018-01-08 23:17:53 +0100

    fixes

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
 db = SQLAlchemy(app)
 
 
+"""
 class User(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(100))
@@ -19,21 +20,24 @@ class User(db.Model):
   def __init__(self, name, email):
     self.name = name
     self.email = email
+"""
 
 
 @app.route('/', methods=['GET'])
 def index():
-  return render_template('index.html', users=User.query.all())
+  return render_template('index.html')
 
 
+"""
 @app.route('/user', methods=['POST'])
 def user():
   u = User(request.form['name'], request.form['email'])
   db.session.add(u)
   db.session.commit()
   return redirect(url_for('index'))
+"""
 
 if __name__ == '__main__':
-  db.create_all()
+  # db.create_all()
   port = int(os.environ.get('PORT', 5000))
   app.run(host='0.0.0.0', port=port, debug=True)
